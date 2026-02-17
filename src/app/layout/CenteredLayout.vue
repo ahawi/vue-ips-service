fc4c0b4d2a48ffae11d5e07f5136a82606f755e9
ahawi <nastya.murlla@gmail.com>
2026-02-17 13:39:35 +0300

    add layout

---
+++ src/app/layout/CenteredLayout.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="center">
+    <div class="card">
+      <RouterView/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.center {
+  min-width: 100dvh;
+  padding: 4em;
+  background: #f4f6f8;
+}
+
+.card {
+  width: 48rem;
+}
+</style>
--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { CenteredLayout } from '../layout'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/auth',
+      name: 'auth',
+      children: [],
+      component: CenteredLayout,
+    },
+  ],
 })
 
 export default router
