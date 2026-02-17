bfcbb47e4ca9a4b2421171b53bf3c27a47003e37
ahawi <nastya.murlla@gmail.com>
2026-02-17 14:38:51 +0300

    change router format

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { CenteredLayout } from '../layout'
+import { AUTH_SECTION_ROUTE } from '@/pages/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/auth',
-      name: 'auth',
-      children: [],
+      ...AUTH_SECTION_ROUTE,
       component: CenteredLayout,
     },
   ],
