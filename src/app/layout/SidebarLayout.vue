3931374ec4c782eb746de4e7f7ad3c49fce88076
ahawi <nastya.murlla@gmail.com>
2026-02-23 18:03:57 +0300

    added sidebar

---
+++ src/app/layout/SidebarLayout.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup>
+import { MAIN_LINK } from '@/shared/config/'
+import type { RouteLocationRaw } from 'vue-router'
+
+interface Link {
+  to: RouteLocationRaw
+  name: string
+}
+
+const LINKS = [
+  {
+    to: MAIN_LINK,
+    name: 'Главная'
+  }
+] as const satisfies Array<Link>
+</script>
+
+<template>
+  <div class="wrapper">
+    <aside class="aside">
+      <nav class="nav">
+        <RouterLink
+          v-for="{ to, name } in LINKS"
+          :to
+          :key="name"
+          >{{ name }}</RouterLink
+        >
+      </nav>
+    </aside>
+
+    <div class="content">
+      <header class="header">
+        <div class="brand">ips-service</div>
+        <div class="hstack">
+          <div class="small">Hi, User</div>
+          <RouterLink :to="MAIN_LINK">Профиль</RouterLink>
+        </div>
+      </header>
+      <main>
+        <RouterView />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  min-height: 100dvh;
+}
+
+.content {
+  flex: 1;
+}
+
+.nav-link {
+  display: block;
+  color: inherit;
+  padding: 0.8rem 0;
+  font-size: 1.6rem;
+  color: #cdb5e1;
+}
+
+.nav-link.router-link-active {
+  color: #fff;
+  font-weight: 700;
+}
+</style>
