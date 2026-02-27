bc7b044c354ea2ab7a15e43fb54b653af774fbb5
ahawi <nastya.murlla@gmail.com>
2026-02-27 17:20:09 +0300

    took out logout from httpClient

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { http } from '@/shared/api'
 
 import App from './App.vue'
 import { router } from './router'
 import './styles/main.css'
+import { useUserStore } from '@/entities/user'
+
+const unAuthorizedHandler = (): void => {
+  const { resetUser } = useUserStore()
+
+  resetUser()
+}
+
+http.onUnAuthorized(unAuthorizedHandler)
 
 const app = createApp(App)
 
